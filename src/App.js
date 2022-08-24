76b04b328c7a0d528956764bf0b2d68767aee9f1
Andrew Kal <andrewkalgit@gmail.com>
2022-08-24 04:06:05 -0400

    Nav Bar UI in progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Search from './Search';
+import SupriseMe from './SupriseMe';
+import Recipes from './Recipes';
+import About from './About';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+      <Route path='home' element={<><Home></Home><Search></Search></>}></Route>
+      <Route path='suprise-me' element={<><SupriseMe></SupriseMe></>}></Route>
+      <Route path='recipes' element={<><Recipes></Recipes></>}></Route>
+      <Route path='about' element={<><About></About></>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function SupriseMe(props) {
+function Home(props) {
     return (
         <div>
-            
+            Homey
         </div>
     );
 }
 
-export default SupriseMe;
+export default Home;
