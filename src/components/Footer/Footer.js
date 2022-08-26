66a276e0dc238ad85ccc643dbc32ed5ea575c889
joesphchang <joesphschang@gmail.com>
2022-08-26 16:30:49 -0400

    started footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Footer() {
+
   return (
-    <div>Footer</div>
-  )
+		<div>
+			<div>
+				<h3>Andrew Kai</h3>
+				<a href='https://github.com/AndrewKalGit'>
+					<i class='devicon-github-original colored'></i>
+				</a>
+				<a href='https://www.linkedin.com/in/andrew-kal/'>
+					<i class='devicon-linkedin-plain colored'></i>
+				</a>
+			</div>
+			<div>
+				<h3>Joey Chang</h3>
+				<a href='https://github.com/joesphchang'>
+					<i class='devicon-github-original colored'></i>
+				</a>
+				<a href='https://www.linkedin.com/in/joesphchang/'>
+					<i class='devicon-linkedin-plain colored'></i>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Footer
